918af4962c43a44a33ba524c36203891baa59f2b
aust1inn <austinotieno364@gmail.com>
2022-04-08 11:53:20 +0300

    creates quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './component/quote-form/quote-form.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { QuotesComponent } from './component/quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    HighlightDirective
+    HighlightDirective,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
