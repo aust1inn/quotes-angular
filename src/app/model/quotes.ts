bd7d4d2fa2aafbe95b4f3908eaf45d62458f93ce
aust1inn <austinotieno364@gmail.com>
2022-04-09 16:15:14 +0300

    Auto-generated commit [ci skip]

--- src/app/model/quotes.ts
+++
@@ -1,5 +0,0 @@
-export class Quotes {
-    constructor (public id:number, public name: string, public author: string,  public quoteWords: string, public upvote:number, public downvote:number, public creationDate: Date) {
-        
-    }
-}
--- src/app/pipes/days-past.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DaysPastPipe } from './days-past.pipe';
-
-describe('DaysPastPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DaysPastPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
