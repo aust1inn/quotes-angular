5a1520fc0d590bfc58cc31afbae7241239823df3
aust1inn <austinotieno364@gmail.com>
2022-04-09 10:42:05 +0300

    style:sets the font to Patrick Hand

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, ElementRef } from '@angular/core';
+import { Quotes } from '../model/quotes';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  
   constructor(private el:ElementRef) {
     this.el.nativeElement.style.backgroundColor="yellow"
    }
